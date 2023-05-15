29a67f1375ff5b4bc4a3dce543ebd58410e03b0c
Carlos Henrique Brasil <91640647+mastercarlosbr01@users.noreply.github.com>
2023-05-15 08:18:04 -0300

    Adicionando exercicios

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            print "Olá mundo!\u{1F480}"; //codepoint
+        ?>
+    </h1>
+    <p>Vamos livrar da maldição</p>
+    
+</body>
+</html>
